d327569825c9d93143e7e77484dbc3a5413c5fad
Muzi <muzikhubeka@gmail.com>
2018-04-13 10:37:18 +0200

    13042018

---
+++ .history/src/app/app.module_20180413092709.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpModule }    from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthGuard } from './shared';
+import { AternityProvider } from '../providers/aternity_api'; 
+import { BlueprismModel} from './models/BlueprismModel';
+import { BusinessAreaModel} from './models/business_area_model';
+import { BlueprismService} from './services/BlueprismService';
+import { DataLoadService } from './services/DataLoadService';
+import { BusinessService} from './services/business_area_service';
+import { OdataLabels} from './models/OdataLabels';
+
+export function createTranslateLoader(http: HttpClient) {
+
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+    imports: [
+        HttpModule,
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient]
+            }
+        }),
+        AppRoutingModule
+    ],
+    declarations: [AppComponent],
+    providers: [
+         AuthGuard, 
+         BlueprismService,
+         BusinessService,
+         DataLoadService,
+         OdataLabels,
+         BlueprismModel
+         ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ .history/src/app/models/BlueprismModel_20180413091817.ts
@@ -0,0 +1,24 @@
+import * as models from '../models/models';
+
+export class BlueprismModel  {
+
+    /** Host REsources */
+    CPU_UTILIZATION_AVG : number;
+    DISK_IO_READ_AVG : number;
+    DISK_IO_WRITE_AVG : number;
+    DISK_QUEUE_LENGTH_MAX : number;
+    HOUR_RUNNING_TOTAL : number;
+    NETWORK_READ_AVG : number;
+    NETWORK_WRITE_AVG : number;
+    PHYSICAL_MEMORY_UTIL_AVG : number;
+    SERVING_DEVICE_NAME : string;
+    TIMEFRAME : Date;
+    VIRTUAL_MEMORY_UTIL_AVG : number;
+    USERNAME : string;
+    ACCOUNT_NAME : string;
+
+
+    set = new Set ([]);
+
+}
+
